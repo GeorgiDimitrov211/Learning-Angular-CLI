263e9d9a9c00d6da5be687e5b105d0a6651918a7
Victor Mejia <victor.a.mejia@gmail.com>
2017-12-02 13:37:12 -0800

    sample generated directive

---
+++ src/app/shared/directives/non-numeric.directive.ts
@@ -0,0 +1,10 @@
+import { Directive } from '@angular/core';
+
+@Directive({
+  selector: '[cmNonNumeric]'
+})
+export class NonNumericDirective {
+
+  constructor() { }
+
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AvatarComponent } from './avatar/avatar.component';
+import { NonNumericDirective } from './directives/non-numeric.directive';
 
 @NgModule({
   imports: [
     CommonModule
   ],
-  declarations: [AvatarComponent]
+  declarations: [AvatarComponent, NonNumericDirective]
 })
 export class SharedModule { }
